776b5e679d6835e46ae26e8894a76981b4bd08a7
Alex <sunsanich1@gmail.com>
2021-01-22 11:11:48 +0200

    Start

---
+++ src/test/java/stepDefs/ExampleSteps.java
@@ -0,0 +1,12 @@
+package stepDefs;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+
+public class ExampleSteps {
+
+    @Given("I am an anonymous customer with clear cookies")
+    public void iAmAnAnonymousCustomerWithClearCookies() {
+
+    }
+}
